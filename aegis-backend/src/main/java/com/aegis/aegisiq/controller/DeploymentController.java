3ac9cf66f9872421003b250d52ae590f7ebf948c
Sabariswaran M <sabariaug2006@gmail.com>
2026-09-18 15:03:34 +0530

    DeploymentController.java

--- aegis-backend/src/main/java/com/aegis/aegisiq/controller/DeploymentController.java
+++ aegis-backend/src/main/java/com/aegis/aegisiq/controller/DeploymentController.java
@@ -1,5 +1,14 @@
 package com.aegis.aegisiq.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/deployment")
 public class DeploymentController {
 
+    @GetMapping("/status")
+    public ResponseEntity<?> getDeploymentStatus() {
+        return ResponseEntity.ok("Deployment optimizer is active");
+    }
 }
